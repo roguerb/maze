c15c5907036ff9ec20cd9929c8c4d8c391d5f833
Randy Coulman <rcoulman@gmail.com>
2013-07-20 17:01:48 -0700

    Add the original problem's tests as an acceptance spec.
    * Skeleton maze class
    * All acceptance specs marked pending until they can pass

---
+++ lib/maze.rb
@@ -0,0 +1,13 @@
+class Maze
+  def initialize(maze_string)
+
+  end
+
+  def solvable?
+    false
+  end
+
+  def steps
+    0
+  end
+end
---
+++ spec/acceptance_spec.rb
@@ -0,0 +1,74 @@
+require "rspec"
+require "maze"
+
+describe "Acceptance Test" do
+  MAZE1 = <<-END_OF_MAZE
+#####################################
+# #   #     #A        #     #       #
+# # # # # # ####### # ### # ####### #
+# # #   # #         #     # #       #
+# ##### # ################# # #######
+#     # #       #   #     # #   #   #
+##### ##### ### ### # ### # # # # # #
+#   #     #   # #   #  B# # # #   # #
+# # ##### ##### # # ### # # ####### #
+# #     # #   # # #   # # # #       #
+# ### ### # # # # ##### # # # ##### #
+#   #       #   #       #     #     #
+#####################################
+  END_OF_MAZE
+
+  MAZE2 = <<-END_OF_MAZE
+#####################################
+# #       #             #     #     #
+# ### ### # ########### ### # ##### #
+# #   # #   #   #   #   #   #       #
+# # ###A##### # # # # ### ###########
+#   #   #     #   # # #   #         #
+####### # ### ####### # ### ####### #
+#       # #   #       # #       #   #
+# ####### # # # ####### # ##### # # #
+#       # # # #   #       #   # # # #
+# ##### # # ##### ######### # ### # #
+#     #   #                 #     #B#
+#####################################
+  END_OF_MAZE
+
+  MAZE3 = <<-END_OF_MAZE
+#####################################
+# #   #           #                 #
+# ### # ####### # # # ############# #
+#   #   #     # #   # #     #     # #
+### ##### ### ####### # ##### ### # #
+# #       # #  A  #   #       #   # #
+# ######### ##### # ####### ### ### #
+#               ###       # # # #   #
+# ### ### ####### ####### # # # # ###
+# # # #   #     #B#   #   # # #   # #
+# # # ##### ### # # # # ### # ##### #
+#   #         #     #   #           #
+#####################################
+  END_OF_MAZE
+
+  let(:maze1) { Maze.new(MAZE1) }
+  let(:maze2) { Maze.new(MAZE2) }
+  let(:maze3) { Maze.new(MAZE3) }
+
+  it "determines if a maze is solvable" do
+    pending "Acceptance tests aren't passing yet"
+    expect(maze1).to be_solvable
+    expect(maze2).to be_solvable
+    expect(maze3).not_to be_solvable
+  end
+
+  it "counts the steps required to solve the maze" do
+    pending "Acceptance tests aren't passing yet"
+    expect(maze1.steps).to eq(44)
+    expect(maze2.steps).to eq(75)
+  end
+
+  it "returns 0 steps for an unsolveable maze" do
+    pending "Acceptance tests aren't passing yet"
+    expect(maze3.steps).to eq(0)
+  end
+end
