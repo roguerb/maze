d119254d8214edee0de9bf9af8e47af235acbc91
Randy Coulman <rcoulman@gmail.com>
2013-07-20 17:27:27 -0700

    Add very basic maze parsing and cell access.

--- lib/maze.rb
+++ lib/maze.rb
@@ -1,6 +1,6 @@
 class Maze
   def initialize(maze_string)
-
+    parse(maze_string)
   end
 
   def solvable?
@@ -9,5 +9,18 @@ class Maze
 
   def steps
     0
+  end
+
+  def at(x, y)
+    return nil unless (0...@cells.size).include?(y)
+    row = @cells[y]
+    return nil unless (0...row.size).include?(x)
+    row[x]
+  end
+
+  private
+
+  def parse(maze_string)
+    @cells = maze_string.lines.map(&:chomp).to_a
   end
 end
---
+++ spec/maze_spec.rb
@@ -0,0 +1,34 @@
+require "rspec"
+require "maze"
+
+describe Maze do
+  MAZE = <<-END_OF_MAZE
+#####
+#A B#
+#####
+  END_OF_MAZE
+
+  let(:maze) { Maze.new(MAZE) }
+
+  it "has walls" do
+    expect(maze.at(0, 0)).to eq("#")
+  end
+
+  it "has halls" do
+    expect(maze.at(2, 1)).to eq(" ")
+  end
+
+  it "has a start point" do
+    expect(maze.at(1, 1)).to eq("A")
+  end
+
+  it "has an end point" do
+    expect(maze.at(3, 1)).to eq("B")
+  end
+
+  it "returns nil for off-grid cell indices" do
+    expect(maze.at(-1, -1)).to be_nil
+    expect(maze.at(2, 3)).to be_nil
+    expect(maze.at(5, 1)).to be_nil
+  end
+end
