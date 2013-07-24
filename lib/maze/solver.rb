817a28c2cc98013c806250f1f41e78d27f175fa5
Randy Coulman <rcoulman@gmail.com>
2013-07-23 17:19:00 -0700

    Extract a queue-like private API in solver.

--- lib/maze/solver.rb
+++ lib/maze/solver.rb
@@ -1,15 +1,34 @@
 require_relative "path"
 
 class Solver
+  def initialize
+    @paths = []
+  end
+
   def solve(maze)
-    paths = [Path.new(maze.starting_cell)]
-    while !paths.empty? do
-      path = paths.shift
+    enqueue(Path.new(maze.starting_cell))
+
+    while !done? do
+      path = next_path
       return path if path.complete?
+
       path.current_cell.traversable_neighbors.each do |neighbor|
-        paths << path.with(neighbor) unless path.include?(neighbor)
+        enqueue(path.with(neighbor)) unless path.include?(neighbor)
       end
     end
     Path.new
+  end
+
+  private
+  def done?
+    @paths.empty?
+  end
+
+  def enqueue(path)
+    @paths << path
+  end
+
+  def next_path
+    @paths.shift
   end
 end
